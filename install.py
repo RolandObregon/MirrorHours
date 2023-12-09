53bebd99e0caf59e74a8436e0ef67afffe239ff7
Rolando <139822458+RolandObregon@users.noreply.github.com>
2023-12-09 16:33:18 -0600

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-import shutil
-import getpass
-import os
-USER_NAME = getpass.getuser()
-destination = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-app = os.path.abspath(os.getcwd()) + "\main.py"
-shutil.move(app, destination)
-# made for windows so start up folder won´t work on mac/linux
