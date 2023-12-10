9f57badda367953fb6f609333acf4fc801a2e06a
Rolando <139822458+RolandObregon@users.noreply.github.com>
2023-12-09 19:39:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import time
 while True:
     hora = dt.today().strftime('%I:%M')
     triplehour = {"1":"11", "2":"22", "3":"33", "4":"44", "5":"55"}
-    if dt.today().strftime('%I:%M %P') == "12:13 AM":
-        noti.notify(title="Good Night", message="Go to sleep", app_name="MirrorHour")
-        break
     if dt.today().strftime('%I') == dt.today().strftime('%M'):
         noti.notify(title=hora, message="<3", app_name="MirrorHour")
         time.sleep(59)
