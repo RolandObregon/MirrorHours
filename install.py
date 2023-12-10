5da8b6b22ade0028cf9196fa2e070e56844ba56a
Rolando <139822458+RolandObregon@users.noreply.github.com>
2023-12-09 18:53:55 -0600

    Sets up main.py
    Places main.py in startup folder and installs needed dependencies

---
+++ install.py
@@ -0,0 +1,18 @@
+import shutil
+import getpass
+import os
+import subprocess
+import sys
+USER_NAME = getpass.getuser()
+destination = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+app = os.path.abspath(os.getcwd()) + "\main.py"
+try:
+    shutil.move(app, destination)
+except:
+    "Error while moving main.py to the startup folder"
+# made for windows so start up folder won´t work on mac/linux
+try:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "plyer"])
+except:
+    "Error while installing plyer or it has been already installed"
+
